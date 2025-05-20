07255f7a049183f6b8abe952da81e2e8a17c1389
Ovanesyanaz <ovanesyanaz@mail.ru>
2025-05-20 21:55:15 +0300

    fix Typemap

--- hw/TypeMap.cpp
+++ hw/TypeMap.cpp
@@ -1,134 +1,162 @@
-#include <iostream>
-#include <string>
-#include <tuple>
 #include <type_traits>
+#include <string>
+#include <vector>
+#include <optional>
+#include <any>
+#include <iostream>
 
-// Исходная реализация TypeList без изменений
-namespace TypeListUtils {
-    template<typename... Types> struct TypeList {};
-    
-    template<typename TList> struct Size;
-    template<typename... Types> struct Size<TypeList<Types...>> {
+namespace TypeListDetail {
+    template <typename... Types>
+    struct TypeList {};
+
+    template <typename... Types>
+    struct Size;
+
+    template <typename... Types>
+    struct Size<TypeList<Types...>> {
         static constexpr std::size_t value = sizeof...(Types);
     };
-    
-    template<typename TList, std::size_t Index> struct TypeAt;
-    template<std::size_t Index> struct TypeAt<TypeList<>, Index> {
-        static_assert(Index < 0, "Index out of bounds");
+
+    template <std::size_t N, typename TypeList>
+    struct TypeAt;
+
+    template <std::size_t N, typename Head, typename... Tail>
+    struct TypeAt<N, TypeList<Head, Tail...>> {
+        using type = typename TypeAt<N - 1, TypeList<Tail...>>::type;
     };
-    template<typename Head, typename... Tail> struct TypeAt<TypeList<Head, Tail...>, 0> {
+
+    template <typename Head, typename... Tail>
+    struct TypeAt<0, TypeList<Head, Tail...>> {
         using type = Head;
     };
-    template<typename Head, typename... Tail, std::size_t Index> 
-    struct TypeAt<TypeList<Head, Tail...>, Index> {
-        using type = typename TypeAt<TypeList<Tail...>, Index - 1>::type;
+
+    template <typename T, typename TypeList>
+    struct Contains;
+
+    template <typename T, typename... Types>
+    struct Contains<T, TypeList<Types...>> {
+        static constexpr bool value = (std::is_same_v<T, Types> || ...);
     };
-    
-    template<typename TList, typename T> struct Contains;
-    template<typename T> struct Contains<TypeList<>, T> {
-        static constexpr bool value = false;
+
+    template <typename T, typename TypeList, std::size_t Index = 0>
+    struct IndexOf;
+
+    template <typename T, typename Head, typename... Tail, std::size_t Index>
+    struct IndexOf<T, TypeList<Head, Tail...>, Index> {
+        static constexpr std::size_t value = std::is_same_v<T, Head> ? Index : IndexOf<T, TypeList<Tail...>, Index + 1>::value;
     };
-    template<typename Head, typename... Tail, typename T> 
-    struct Contains<TypeList<Head, Tail...>, T> {
-        static constexpr bool value = std::is_same_v<Head, T> || Contains<TypeList<Tail...>, T>::value;
-    };
-    
-    template<typename TList, typename T, std::size_t Index = 0> struct IndexOf;
-    template<typename T, std::size_t Index> struct IndexOf<TypeList<>, T, Index> {
+
+    template <typename T, std::size_t Index>
+    struct IndexOf<T, TypeList<>, Index> {
         static constexpr std::size_t value = static_cast<std::size_t>(-1);
     };
-    template<typename Head, typename... Tail, typename T, std::size_t Index> 
-    struct IndexOf<TypeList<Head, Tail...>, T, Index> {
-        static constexpr std::size_t value = 
-            std::is_same_v<Head, T> ? Index : IndexOf<TypeList<Tail...>, T, Index + 1>::value;
+
+    template <typename T, typename TypeList>
+    struct PushBack;
+
+    template <typename T, typename... Types>
+    struct PushBack<T, TypeList<Types...>> {
+        using type = TypeList<Types..., T>;
     };
-} // namespace TypeListUtils
 
-// Реализация TypeMap
-template<typename... Keys>
-class TypeMap {
-private:
-    // Хранилище значений для каждого типа ключа
-    std::tuple<Keys...> values;
-    
-    // Получение индекса типа в списке ключей
-    template<typename Key>
-    static constexpr std::size_t getKeyIndex() {
-        constexpr std::size_t index = TypeListUtils::IndexOf<TypeListUtils::TypeList<Keys...>, Key>::value;
-        static_assert(index != static_cast<std::size_t>(-1), "Key not found in TypeMap");
-        return index;
-    }
+    template <typename T, typename TypeList>
+    struct PushFront;
 
-public:
-    // Добавление значения по типу ключа
-    template<typename Key>
-    void AddValue(Key value) {
-        std::get<getKeyIndex<Key>()>(values) = value;
-    }
-    
-    // Получение значения по типу ключа
-    template<typename Key>
-    Key& GetValue() {
-        return std::get<getKeyIndex<Key>()>(values);
-    }
-    
-    template<typename Key>
-    const Key& GetValue() const {
-        return std::get<getKeyIndex<Key>()>(values);
-    }
-    
-    // Проверка наличия типа ключа
-    template<typename Key>
-    constexpr bool Contains() const {
-        return TypeListUtils::Contains<TypeListUtils::TypeList<Keys...>, Key>::value;
-    }
-    
-    // Удаление значения (сброс в значение по умолчанию)
-    template<typename Key>
-    void RemoveValue() {
-        std::get<getKeyIndex<Key>()>(values) = Key{};
-    }
-};
-
-// Пример использования
-struct DataA {
-    std::string value;
-    friend std::ostream& operator<<(std::ostream& os, const DataA& data) {
-        return os << data.value;
-    }
-};
-
-struct DataB {
-    int value;
-    friend std::ostream& operator<<(std::ostream& os, const DataB& data) {
-        return os << data.value;
-    }
-};
-
-int main() {
-    // Создаем TypeMap с указанием типов ключей
-    TypeMap<int, double, DataA, DataB> myTypeMap;
-    
-    // Добавление элементов
-    myTypeMap.AddValue<int>(42);
-    myTypeMap.AddValue<double>(3.14);
-    myTypeMap.AddValue<DataA>({"Hello, TypeMap!"});
-    myTypeMap.AddValue<DataB>({10});
-    
-    // Получение и вывод значений
-    std::cout << "Value for int: " << myTypeMap.GetValue<int>() << std::endl;
-    std::cout << "Value for double: " << myTypeMap.GetValue<double>() << std::endl;
-    std::cout << "Value for DataA: " << myTypeMap.GetValue<DataA>().value << std::endl;
-    std::cout << "Value for DataB: " << myTypeMap.GetValue<DataB>().value << std::endl;
-    
-    // Проверка наличия
-    std::cout << "Contains int? " << (myTypeMap.Contains<int>() ? "Yes" : "No") << std::endl;
-    
-    // Удаление элемента
-    myTypeMap.RemoveValue<double>();
-    
-    // Попытка получения удаленного элемента
-    std::cout << "Value for double after removal: " << myTypeMap.GetValue<double>() << std::endl;
-    
-    return 0;
+    template <typename T, typename... Types>
+    struct PushFront<T, TypeList<Types...>> {
+        using type = TypeList<T, Types...>;
+    };
 }
+
+template <typename... Types>
+using TypeList = TypeListDetail::TypeList<Types...>;
+
+template<typename... Args>
+struct TypeMap;
+
+template<typename Head, typename... Tail>
+struct TypeMap<Head, Tail...>
+{
+    TypeMap()
+    {
+        using keys = TypeList<Head, Tail...>;
+        values.resize(sizeof...(Tail)+1);
+    };
+
+    template<typename T> void addValue(T value)
+    {
+        constexpr int i = TypeListDetail::IndexOf<T, keys>::value;
+        values[i] = std::make_optional(std::any(value));
+    };
+
+    template<typename T> T getValue()
+    {
+        int i = TypeListDetail::IndexOf<T, keys>::value;
+        if (values[i]) {
+            T from_any = std::any_cast<T>(values[i].value());
+            return from_any;
+        }
+        
+        throw std::invalid_argument( "getValue(): value for this key doesn`t exists" );
+    };
+    
+    template<typename T> bool valueExists()
+    {
+        int i = TypeListDetail::IndexOf<T, keys>::value;
+        return values[i].has_value();
+    }
+
+    template<typename T> bool Contains()
+    {
+        return TypeListDetail::Contains<T, keys>::value;
+    }
+    
+    template<typename T> void removeValue()
+    {
+        constexpr int i = TypeListDetail::IndexOf<T, keys>::value;
+        values[i] = std::nullopt;
+    };
+
+    typedef TypeList<Head, Tail...> keys;
+    std::vector<std::optional<std::any>> values;
+};
+
+int main()
+{
+    struct MyType1 {
+        std::string value;
+    };
+    struct MyType2 {
+        int value;
+    };
+    
+    TypeMap<int, MyType1, double, MyType2> myTypeMap;
+    
+    myTypeMap.addValue<double>(2.72);
+    myTypeMap.addValue<MyType1>({"Hello, World!"});
+    myTypeMap.addValue<int>(52);
+    myTypeMap.addValue<MyType2>({13});
+
+    std::cout << "Value for int: "<<myTypeMap.getValue<int>() << std::endl;
+    std::cout << "Value for double: "
+              << myTypeMap.getValue<double>() << std::endl;
+    std::cout << "Value for MyType1: "
+              << myTypeMap.getValue<MyType1>().value << std::endl;
+    std::cout << "Value for MyType2: "
+              << myTypeMap.getValue<MyType2>().value << std::endl;
+
+    std::cout << "Contains int? "
+              << (myTypeMap.Contains<int>() ? "Yes" : "No") << std::endl;
+              
+    std::cout << "Value for double exists before removal: "
+              << (myTypeMap.valueExists<double>() ? "Yes" : "No") << std::endl;
+
+    myTypeMap.removeValue<double>();
+    
+    std::cout << "Value for double exists after removal: "
+              << (myTypeMap.valueExists<double>() ? "Yes" : "No") << std::endl;
+
+    std::cout << "Try get value for double after removal: "
+              << myTypeMap.getValue<double>() << std::endl;
+    return 0;
+};
